29aa5723df46f6332c1c8fcc5b05582bff3521fb
Isaac Bruno do Vale Souza <20211074010034@ifrn.local>
2024-11-14 07:59:45 -0300

    CartContext OK. Faltando Chechout.js

---
+++ components/Shop.js
@@ -0,0 +1,31 @@
+import {useContext} from 'react';
+import { CartContext } from "../context/CartContext";
+import Product from "./Product";
+import { CircularProgress } from "@mui/material";
+
+export default function Shop() {
+const { products, loading, error } = useContext(CartContext); 
+
+    return (
+        <section id="shop">
+            <h2>Elegant Products for Everyone</h2>
+
+            <ul id="products">
+                {error && <p>{error}</p>}
+                {!loading && products ? (
+                    products.map((product) => (
+                        <li key={product.id}>
+                            <Product {...product} />
+                        </li>
+                    ))
+                ) : (
+                    <div id="loading">
+                        <CircularProgress size="10rem" color="inherit" />
+                        <p>Loading products...</p>
+                    </div>
+                )}
+            </ul>
+
+        </section>
+    );
+}
